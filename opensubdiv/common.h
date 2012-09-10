b3d0296c7d34d9519605a6b2da55581a6409f458
Andrew Wong <andrew.kw.w@gmail.com>
2012-09-09 22:49:43 -0400

    osd: Add OSD_API define for exporting symbols on Windows
    Clients of the OSD library will need to define OSD_DLL if they want to
    dynamically link against the OSD library.

--- opensubdiv/common.h
+++ opensubdiv/common.h
@@ -60,4 +60,24 @@
 
 #define OPENSUBDIV_VERSION RELEASE_CANDIDATE_1_0
 
+#ifdef WIN32
+    #ifdef OSD_DLL
+        #ifdef OSD_INTERNAL
+            #define OSD_API __declspec(dllexport)
+            #define OSD_TEMPLATE
+        #else
+            #define OSD_API __declspec(dllimport)
+            #define OSD_TEMPLATE extern
+        #endif
+    #endif
+#endif
+
+#ifndef OSD_API
+    #define OSD_API
+#endif
+
+#ifndef OSD_TEMPLATE
+    #define OSD_TEMPLATE
+#endif
+
 #endif /* OPENSUBDIV_COMMON_H */
